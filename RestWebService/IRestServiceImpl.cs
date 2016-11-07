91d5a3141becf58361ca022aaa03248fa785772c
Cagdas Korkut <c.korkut@dhbbank.com>
2016-11-07 13:57:16 +0200

    added web service project into solution
    initial commit of web service

---
+++ RestWebService/IRestServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestWebService
+{
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string GetAllCountdowns(string id);
+    }
+}
---
+++ RestWebService/RestServiceImpl.svc.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWebService
+{
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        public string GetAllCountdowns(string id)
+        {
+            return "test deneme";
+        }
+    }
+}
